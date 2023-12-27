e8abf6636a29143a0ac5399745304663182bd729
wafi <34021031+wafi-izz@users.noreply.github.com>
2023-12-28 01:51:42 +0300

    28-12-2023

---
+++ Template.DataStructure/templates/Base/IMigratablesDictionary.cs
@@ -0,0 +1,15 @@
+﻿using Ember.DataScheme.Schemas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace templates.Base;
+
+internal interface IMigratablesDictionary
+{
+    public Schema Schema { get; set; }
+    void Up();
+    void Down();
+}
---
+++ Template.DataStructure/templates/Program.cs
@@ -0,0 +1,41 @@
+﻿using templates.Base;
+using templates.Base.DatabaseObjects;
+using Ember.Transcription;
+using Ember.DataScheme.Schemas;
+using System;
+using System.Linq;
+
+namespace templates
+{
+    public class Init
+    {
+        public Init()
+        {
+
+        }
+        public static String Main()
+        {
+            DatabaseObjectScrapper Scrapper = new DatabaseObjectScrapper();
+            Schema Schema = new Schema(Scrapper.TableList);
+
+            var MigrationTypes = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(x => typeof(IMigratablesDictionary).IsAssignableFrom(x))
+                .Where(x => x.Namespace!.StartsWith($"{nameof(Ember)}.{nameof(DataStructure)}.{nameof(Migrations)}"))
+                .Where(x => x.IsInterface == false)
+                .OrderBy(x => x.Namespace)
+                .ToList();
+            foreach (var type in MigrationTypes)
+            {
+                IMigratablesDictionary Migration = (IMigratablesDictionary)Activator.CreateInstance(type)!;
+                var t = type.GetType().Name;
+                Migration.Schema = Schema;
+                Migration.Down();
+                Migration.Up();
+            }
+            Transcriber Transcriber = new Transcriber(Schema, SqlTypeEnum.SqlServer);
+            String GeneratedScript = Transcriber.Transcribe();
+            return GeneratedScript; //"a generated database script ... hopefully"
+        }
+    }
+}
